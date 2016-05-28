62e4127f931386f6d13fde677582a75aab3b9ee8
DmitrDmitry <Dzhariashvili0706@yandex.ru>
2016-05-29 01:17:28 +0300

    Add files via upload

---
+++ IVTp/2014/Dzariashvili/5 5.py
@@ -0,0 +1,20 @@
+# Задача 5, Вариант 5
+
+import random
+
+sobor = random.randint(1, 7)
+
+if (sobor) == 1:
+    print ("Благовещенский собор ")
+elif (sobor) == 2:
+    print ("Архангельский собор ")
+elif (sobor) == 3:
+    print ("Успенский собор")
+elif (sobor) == 4:
+    print ("Собор «Двенадцати апостолов»")
+elif (sobor) == 5:
+    print ("Церковь Ризположения")
+elif (sobor) == 6:
+    print ("Колокольня Ивана Великого")
+
+input ("\n\n...")
